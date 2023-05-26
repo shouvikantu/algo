8793983c2a07315ecd9a176a4e3f3dafd5d88a1c
shouvikantu <shouvikwu26@gmail.com>
2023-05-25 22:24:40 -0700

    insertsion Sort

---
+++ insertionsort.py
@@ -0,0 +1,10 @@
+def insertionSort(Arr):
+    for i in range(1,len(Arr)):
+        key = Arr[i]
+        j= i-1
+        while j >=0 and Arr[j]>key:
+            Arr[j+1], Arr[j] = Arr[j], Arr[j+1]
+            j=j-1
+    return Arr
+
+print(insertionSort([4,3,5,2,1,9,3,12,4]))
